64734ff06fa2438be69904ade9ca44115b2afefd
Jefferson Carlos <jeff.carlosbd@gmail.com>
2023-01-28 00:56:56 -0300

    extrai modulo da aplicação

---
+++ invoices-status/src/application.rs
@@ -0,0 +1,10 @@
+use crate::{configurations::Configurations, csv::read_csv, spreadsheet};
+
+pub fn run(configurations: Configurations) -> Result<(), &'static str> {
+    let _spreadsheet = {
+        let structured_rows = read_csv(&configurations.csv_path, &configurations.filter)?;
+        spreadsheet::mount(structured_rows)
+    };
+
+    Ok(())
+}
--- invoices-status/src/lib.rs
+++ invoices-status/src/lib.rs
@@ -1,19 +1,6 @@
+pub mod application;
 pub mod configurations;
 
 mod csv;
 mod spreadsheet;
 mod tools;
-
-pub mod application {
-
-    use crate::{configurations::Configurations, csv::read_csv, spreadsheet};
-
-    pub fn run(configurations: Configurations) -> Result<(), &'static str> {
-        let _spreadsheet = {
-            let structured_rows = read_csv(&configurations.csv_path, &configurations.filter)?;
-            spreadsheet::mount(structured_rows)
-        };
-
-        Ok(())
-    }
-}
