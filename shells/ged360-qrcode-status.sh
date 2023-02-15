18472e2458203178b3a1559a378d3bd0b40e04d4
Jefferson Carlos <jeff.carlosbd@gmail.com>
2023-02-15 14:50:38 -0300

    corrige ordem

--- shells/ged360-qrcode-status.sh
+++ shells/ged360-qrcode-status.sh
@@ -5,10 +5,10 @@ header="Content-Type: application/json"
 data='{"camArquivoQrcode": "NAS:DAAEQAQ51CC5DFEF1A653FB73EE20738", "dataDeExpiracao": "15-02-2023 23:59:59"}'
 response=$(curl --insecure --request POST --url "${endpoint}" --header "${header}" --data "${data}"2>&1)
 
-if [[ "$response" != *"\"type\":\"success\""* ]]; then
-    /webaplic/.bin/tg -845994286 "✅ GED360 [QRCode] retornou sucesso."
+if [[ "${response}" != *"\"type\":\"success\""* ]]; then
+    ./tg -845994286 "⚠️ ATENÇÃO ⚠️"
+    ./tg -845994286 "O GED 360 retornou algo diferente de type=sucesso"
+    ./tg -845994286 "Resposta do endpoint: ${response}"
 else
-    /webaplic/.bin/tg -845994286 "⚠️ ATENÇÃO ⚠️"
-    /webaplic/.bin/tg -845994286 "O GED 360 retornou algo diferente de type=sucesso"
-    /webaplic/.bin/tg -845994286 "Resposta do endpoint: ${response}"
+    ./tg -845994286 "✅ GED360 [QRCode] retornou sucesso."
 fi
