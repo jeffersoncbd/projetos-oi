6d450ce3f74d310e9c0da7c06955cb06adc80ddb
Jefferson Carlos <jeff.carlosbd@gmail.com>
2023-02-15 18:46:57 -0300

    finaliza desenvolvimento do monitor do ged qrcode

--- shells/ged360-qrcode-status.sh
+++ shells/ged360-qrcode-status.sh
@@ -3,10 +3,10 @@
 endpoint="http://ged360.intranet/ws/econtas/get-token"
 header="Content-Type: application/json"
 data='{"camArquivoQrcode": "NAS:DAAEQAQ51CC5DFEF1A653FB73EE20738", "dataDeExpiracao": "15-02-2023 23:59:59"}'
-response=$(curl --insecure --request POST --url "${endpoint}" --header "${header}" --data "${data}"2>&1)
+response=$(curl --insecure --request POST --url "${endpoint}" --header "${header}" --data "${data}" 2>&1)
+
+response=$(tail -n +4 <<< "$response")
 
 if [[ "${response}" != *"\"type\":\"success\""* ]]; then
-    ./tg -845994286 $'⚠️ ATENÇÃO ⚠️\nFalha na conexão com link do GED360 QRCode\nResposta do endpoint:\n'"${response}"
-else
-    ./tg -845994286 "✅ Link do GED360 QRCode retornou sucesso."
+    /webaplic/.bin/tg "-845994286" $'⚠️ ATENÇÃO ⚠️\nFalha na conexão com link do GED360 QRCode\nResposta do endpoint:\n'"${response}"
 fi
