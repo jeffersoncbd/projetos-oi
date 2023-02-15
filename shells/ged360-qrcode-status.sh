4d6368a4a6063b772c4f0ad3de5ce398cb6c1929
Jefferson Carlos <jeff.carlosbd@gmail.com>
2023-02-15 14:35:32 -0300

    cria script de monitoração do GED360 QRCode

---
+++ shells/ged360-qrcode-status.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+endpoint="http://ged360.intranet/ws/econtas/get-token"
+header="Content-Type: application/json"
+data='{"camArquivoQrcode": "NAS:DAAEQAQ51CC5DFEF1A653FB73EE20738", "dataDeExpiracao": "15-02-2023 23:59:59"}'
+response=$(curl --insecure --request POST --url "${endpoint}" --header "${header}" --data "${data}"2>&1)
+
+if [[ "$response" != *"\"type\":\"success\""* ]]; then
+    /webaplic/.bin/tg -845994286 "✅ GED360 [QRCode] retornou sucesso."
+else
+    /webaplic/.bin/tg -845994286 "⚠️ ATENÇÃO ⚠️"
+    /webaplic/.bin/tg -845994286 "O GED 360 retornou algo diferente de type=sucesso"
+    /webaplic/.bin/tg -845994286 "Resposta do endpoint: ${response}"
+fi
