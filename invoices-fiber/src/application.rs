44c5fa2dc493241e008e3a7ff349d6ee2599eb9d
Jefferson Carlos <jeff.carlosbd@gmail.com>
2023-02-23 16:32:41 -0300

    instala e configura telegram

--- invoices-fiber/src/application.rs
+++ invoices-fiber/src/application.rs
@@ -1,9 +1,11 @@
 use crate::{
-    tools::{csv, report},
+    tools::{csv, report, telegram},
     Configurations,
 };
 
 pub fn run(configurations: Configurations) -> Result<(), &'static str> {
+    let _tg = telegram::new_client(configurations.telegram_token);
+
     let csv_content = csv::read(&configurations.csv_path)?;
     {
         let structured_csv = csv::estruture(&csv_content);
--- invoices-fiber/src/configurations.rs
+++ invoices-fiber/src/configurations.rs
@@ -1,5 +1,7 @@
 pub struct Configurations {
     pub csv_path: String,
+    pub telegram_token: String,
+    pub destiny_id: String,
 }
 
 impl Configurations {
@@ -9,6 +11,8 @@ impl Configurations {
         if cfg!(debug_assertions) {
             return Ok(Configurations {
                 csv_path: String::from("model.csv"),
+                telegram_token: String::from("5600687475:AAFtM9o3pcnECnDCMeM42OC0Rb1etknjk_Q"),
+                destiny_id: String::from("5828637972"),
             });
         }
 
@@ -22,6 +26,20 @@ impl Configurations {
             None => return Err("Deve ser informado um arquivo .csv com os dados (em $1)"),
         };
 
-        Ok(Configurations { csv_path })
+        let telegram_token = match args.next() {
+            Some(token) => token,
+            None => return Err("Informe to token do telegram (em $2)"),
+        };
+
+        let destiny_id = match args.next() {
+            Some(id) => id,
+            None => return Err("Informe o ID da conversa que irá receber o relatório (em $3)"),
+        };
+
+        Ok(Configurations {
+            csv_path,
+            telegram_token,
+            destiny_id,
+        })
     }
 }
