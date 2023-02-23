a340a269e5b451a5c464e4949b18609c09f3f3c9
Jefferson Carlos <jeff.carlosbd@gmail.com>
2023-02-23 16:25:14 -0300

    ajusta relatórios para imprimir somente linhas em uso

--- invoices-fiber/src/tools/report.rs
+++ invoices-fiber/src/tools/report.rs
@@ -22,13 +22,14 @@ const NOTIFICATION: [&str; 4] = [
     "PROCESSADO COM FALHA",
 ];
 
-struct Headers<'a> {
-    pub columns: Vec<&'a str>,
+struct Report<'a> {
     pub rows: Vec<&'a str>,
+    pub spreadsheet: Spreadsheet,
+    pub totals: (u32, u32),
 }
 struct Reports<'a> {
-    pub headers: Headers<'a>,
-    pub spreadsheets: HashMap<&'a str, Spreadsheet>,
+    pub column_headers: Vec<&'a str>,
+    pub reports: HashMap<&'a str, Report<'a>>,
 }
 
 fn str_to_date_time(value: &str) -> DateTime<Utc> {
@@ -61,200 +62,264 @@ fn extract_reports<'a>(rows: &'a Vec<Row>) -> Reports<'a> {
     column_headers.sort_by(|a, b| a.1.cmp(&b.1));
     let column_headers = column_headers.iter().map(|h| h.0).collect();
 
-    let mut spreadsheets = HashMap::new();
+    let mut reports = HashMap::new();
 
     for report_name in report_names {
-        spreadsheets.insert(
+        let spreadsheet = Spreadsheet::new(format!(
+            "[Nova Fibra] Relatório de faturas - {}",
+            report_name
+        ));
+        let mut spreadsheet_rows = Vec::new();
+        let mut totals = (0, 0);
+
+        for row in rows {
+            if row.tipo_pro == report_name {
+                if !spreadsheet_rows.contains(&row.status_notificacao) {
+                    spreadsheet_rows.push(&row.status_notificacao)
+                }
+
+                // Caso valor atual seja BACKLOG inclui no total
+                if BACKLOG.contains(&row.status_notificacao.to_uppercase().as_str()) {
+                    let current_value: u32 = row.count.parse().unwrap();
+                    totals.0 = totals.0 + current_value;
+                }
+                // Caso valor atual seja NOTIFICATION inclui no total
+                if NOTIFICATION.contains(&row.status_notificacao.to_uppercase().as_str()) {
+                    let current_value: u32 = row.count.parse().unwrap();
+                    totals.1 = totals.1 + current_value;
+                }
+            }
+        }
+        spreadsheet_rows.sort();
+
+        reports.insert(
             report_name,
-            Spreadsheet::new(format!(
-                "[Nova Fibra] Relatório de faturas - {}",
-                report_name
-            )),
+            Report {
+                rows: spreadsheet_rows,
+                spreadsheet,
+                totals,
+            },
         );
     }
 
     Reports {
-        headers: Headers {
-            columns: column_headers,
-            rows: row_headers,
-        },
-        spreadsheets,
+        column_headers,
+        reports,
     }
 }
 
 pub fn print(rows: Vec<Row>) {
     let mut reports = extract_reports(&rows);
 
-    for (_, spreadsheet) in &mut reports.spreadsheets {
-        spreadsheet.add_in_cell(Cell {
+    for (report_type, report) in &mut reports.reports {
+        let backlog_row_number = report.rows.len() + 4;
+        let notification_row_number = backlog_row_number + 1;
+        let row_headers_column_number = 1;
+        let column_headers_row_number = 1;
+
+        // Imprime todos os títulos das colunas
+        for (i, header) in reports.column_headers.iter().enumerate() {
+            let column_number = i as u32 + 2;
+            report.spreadsheet.set_cell(Cell {
+                column: column_number,
+                row: column_headers_row_number,
+                content: String::from(header.clone()),
+                color: None,
+            });
+            report.spreadsheet.set_column_width(i + 2, 80);
+        }
+        // Ajusta largura da coluna de títulos
+        report
+            .spreadsheet
+            .set_column_width(row_headers_column_number, 230);
+
+        // Imprime todos os títulos das linhas
+        for (i, header) in report.rows.iter().enumerate() {
+            let row_number = i as u32 + 2;
+
+            report.spreadsheet.set_cell(Cell {
+                column: row_headers_column_number as u32,
+                row: row_number,
+                content: String::from(header.clone()),
+                color: None,
+            });
+
+            if report_type != &"Sem tipo" {
+                // Pinta de verde ou amarelo se título for backlog ou notification
+                if BACKLOG.contains(&header.to_uppercase().as_str()) {
+                    report
+                        .spreadsheet
+                        .set_row_color(row_number as usize, Some([0, 176, 80, 255]));
+                }
+                if NOTIFICATION.contains(&header.to_uppercase().as_str()) {
+                    report
+                        .spreadsheet
+                        .set_row_color(row_number as usize, Some([255, 255, 0, 255]));
+                };
+            };
+        }
+
+        if report_type == &"Sem tipo" {
+            continue;
+        }
+
+        // Zera célula do total de backlog
+        report.spreadsheet.add_in_cell(Cell {
             column: 2,
-            row: reports.headers.rows.len() as u32 + 4,
+            row: backlog_row_number as u32,
+            content: String::from("0"),
+            color: None,
+        });
+        // Zera célula do total de notification
+        report.spreadsheet.add_in_cell(Cell {
+            column: 2,
+            row: notification_row_number as u32,
             content: String::from("0"),
             color: None,
         });
 
-        spreadsheet.add_in_cell(Cell {
-            column: 2,
-            row: reports.headers.rows.len() as u32 + 5,
-            content: String::from("0"),
+        // Imprime título backlog
+        report.spreadsheet.set_cell(Cell {
+            column: row_headers_column_number as u32,
+            row: backlog_row_number as u32,
+            content: String::from("BACKLOG"),
             color: None,
         });
+        // Imprime total backlog
+        report.spreadsheet.add_in_cell(Cell {
+            column: row_headers_column_number as u32 + 1,
+            row: backlog_row_number as u32,
+            content: report.totals.0.to_string(),
+            color: None,
+        });
+
+        // Imprime título notification
+        report.spreadsheet.set_cell(Cell {
+            column: row_headers_column_number as u32,
+            row: notification_row_number as u32,
+            content: String::from("NOTIFICATION"),
+            color: None,
+        });
+        // Imprime total notification
+        report.spreadsheet.add_in_cell(Cell {
+            column: row_headers_column_number as u32 + 1,
+            row: notification_row_number as u32,
+            content: report.totals.1.to_string(),
+            color: None,
+        });
+
+        // Formata total BACKLOG
+        {
+            report.spreadsheet.set_cell_color(
+                (row_headers_column_number as u32, backlog_row_number as u32),
+                Some([0, 176, 80, 255]),
+            );
+            report.spreadsheet.set_cell_color(
+                (
+                    row_headers_column_number as u32 + 1,
+                    backlog_row_number as u32,
+                ),
+                Some([0, 176, 80, 255]),
+            );
+        }
+        // Formata total NOTIFICATION
+        {
+            report.spreadsheet.set_cell_color(
+                (
+                    row_headers_column_number as u32,
+                    notification_row_number as u32,
+                ),
+                Some([255, 255, 0, 255]),
+            );
+            report.spreadsheet.set_cell_color(
+                (
+                    row_headers_column_number as u32 + 1,
+                    notification_row_number as u32,
+                ),
+                Some([255, 255, 0, 255]),
+            );
+        }
     }
 
     for row in &rows {
-        let spreadsheet = reports.spreadsheets.get_mut(row.tipo_pro).unwrap();
-        spreadsheet.set_margin(10);
+        let report = reports.reports.get_mut(row.tipo_pro).unwrap();
+        report.spreadsheet.set_margin(10);
 
-        for (i, row_headers) in reports.headers.rows.iter().enumerate() {
-            spreadsheet.set_cell(Cell {
-                column: 1,
-                row: i as u32 + 2,
-                content: String::from(row_headers.clone()),
-                color: None,
-            });
-            if BACKLOG.contains(&row_headers.to_uppercase().as_str()) {
-                spreadsheet.set_row_color(i + 2, Some([0, 176, 80, 255]));
-            }
-            if NOTIFICATION.contains(&row_headers.to_uppercase().as_str()) {
-                spreadsheet.set_row_color(i + 2, Some([255, 255, 0, 255]));
-            }
-        }
-        spreadsheet.set_column_width(1, 230);
+        // ###################### POSIÇÕES DA TABELA ###################### //
+        let row_headers_column_number: usize = 1;
+        let column_headers_row_number: usize = 1;
 
-        for (i, column_headers) in reports.headers.columns.iter().enumerate() {
-            spreadsheet.set_cell(Cell {
-                column: i as u32 + 2,
-                row: 1,
-                content: String::from(column_headers.clone()),
-                color: None,
-            });
-            spreadsheet.set_column_width(i + 2, 80);
-            spreadsheet.set_row_custom_font(1, "bold");
-        }
+        let total_column_number = reports.column_headers.len() + 2;
+        let total_row_number = report.rows.len() + 2;
 
-        let column_number = reports
-            .headers
-            .columns
+        let this_row_number = report
+            .rows
+            .iter()
+            .position(|r| r == &row.status_notificacao)
+            .unwrap() as u32
+            + 2;
+
+        let this_column_number = reports
+            .column_headers
             .iter()
             .position(|h| h == &row.data_venc)
             .unwrap() as u32
             + 2;
-        let row_number = reports
-            .headers
-            .rows
-            .iter()
-            .position(|h| h == &row.status_notificacao)
-            .unwrap() as u32
-            + 2;
-        spreadsheet.add_in_cell(Cell {
-            column: column_number,
-            row: row_number,
+        // ################################################################ //
+
+        // Adiciona na valor atual na célula correspondente
+        report.spreadsheet.add_in_cell(Cell {
+            column: this_column_number,
+            row: this_row_number,
             content: String::from(row.count),
             color: None,
         });
 
-        spreadsheet.set_cell(Cell {
-            column: reports.headers.columns.len() as u32 + 2,
-            row: 1,
+        // Imprime título da coluna Total Geral
+        report.spreadsheet.set_cell(Cell {
+            column: total_column_number as u32,
+            row: column_headers_row_number as u32,
             content: String::from("TOTAL GERAL"),
             color: None,
         });
-        spreadsheet.add_in_cell(Cell {
-            column: reports.headers.columns.len() as u32 + 2,
-            row: row_number,
+        // Adiciona total da linha atual na coluna Total Geral
+        report.spreadsheet.add_in_cell(Cell {
+            column: total_column_number as u32,
+            row: this_row_number as u32,
             content: String::from(row.count),
             color: None,
         });
-        spreadsheet.set_column_custom_font(reports.headers.columns.len() + 2, "bold");
-        spreadsheet.set_column_font_size(reports.headers.columns.len() + 2, 16);
-        spreadsheet.set_column_width(reports.headers.columns.len() + 2, 110);
+        // Formata coluna Total Geral
+        report
+            .spreadsheet
+            .set_column_width(total_column_number, 110);
 
-        spreadsheet.set_cell(Cell {
-            column: 1,
-            row: reports.headers.rows.len() as u32 + 2,
+        // Imprime título da linha TOTAL GERAL
+        report.spreadsheet.set_cell(Cell {
+            column: row_headers_column_number as u32,
+            row: total_row_number as u32,
             content: String::from("TOTAL GERAL"),
             color: None,
         });
-        spreadsheet.add_in_cell(Cell {
-            column: column_number,
-            row: reports.headers.rows.len() as u32 + 2,
+        // Adiciona total da coluna atual na linha Total Geral
+        report.spreadsheet.add_in_cell(Cell {
+            column: this_column_number,
+            row: total_row_number as u32,
             content: String::from(row.count),
             color: None,
         });
-        spreadsheet.set_row_custom_font(reports.headers.rows.len() + 2, "bold");
-        spreadsheet.set_row_font_size(reports.headers.rows.len() + 2, 16);
 
-        spreadsheet.add_in_cell(Cell {
-            column: reports.headers.columns.len() as u32 + 2,
-            row: reports.headers.rows.len() as u32 + 2,
+        // Adiciona total geral de tudo
+        report.spreadsheet.add_in_cell(Cell {
+            column: total_column_number as u32,
+            row: total_row_number as u32,
             content: String::from(row.count),
             color: None,
         });
-        spreadsheet.set_cell_font_size(
-            (
-                reports.headers.columns.len() as u32 + 2,
-                reports.headers.rows.len() as u32 + 2,
-            ),
-            18,
-        );
-
-        spreadsheet.set_cell(Cell {
-            column: 1,
-            row: reports.headers.rows.len() as u32 + 4,
-            content: String::from("BACKLOG"),
-            color: None,
-        });
-        spreadsheet.set_cell_color(
-            (1, reports.headers.rows.len() as u32 + 4),
-            Some([0, 176, 80, 255]),
-        );
-        if BACKLOG.contains(&row.status_notificacao.to_uppercase().as_str()) {
-            spreadsheet.add_in_cell(Cell {
-                column: 2,
-                row: reports.headers.rows.len() as u32 + 4,
-                content: String::from(row.count),
-                color: None,
-            });
-        }
-        spreadsheet.set_cell_color(
-            (2, reports.headers.rows.len() as u32 + 4),
-            Some([0, 176, 80, 255]),
-        );
-
-        spreadsheet.set_cell(Cell {
-            column: 1,
-            row: reports.headers.rows.len() as u32 + 5,
-            content: String::from("NOTIFICATION"),
-            color: None,
-        });
-        spreadsheet.set_cell_color(
-            (1, reports.headers.rows.len() as u32 + 5),
-            Some([255, 255, 0, 255]),
-        );
-        if NOTIFICATION.contains(&row.status_notificacao.to_uppercase().as_str()) {
-            spreadsheet.add_in_cell(Cell {
-                column: 2,
-                row: reports.headers.rows.len() as u32 + 5,
-                content: String::from(row.count),
-                color: None,
-            });
-        }
-        spreadsheet.set_cell_color(
-            (2, reports.headers.rows.len() as u32 + 5),
-            Some([255, 255, 0, 255]),
-        );
-
-        spreadsheet.set_row_custom_font(reports.headers.rows.len() + 4, "bold");
-        spreadsheet.set_row_font_size(reports.headers.rows.len() + 4, 20);
-        spreadsheet.set_row_height(reports.headers.rows.len() + 4, 25);
-
-        spreadsheet.set_row_custom_font(reports.headers.rows.len() + 5, "bold");
-        spreadsheet.set_row_font_size(reports.headers.rows.len() + 5, 20);
-        spreadsheet.set_row_height(reports.headers.rows.len() + 5, 25);
     }
 
-    for (report_name, spreadsheet) in reports.spreadsheets {
+    for (report_name, report) in reports.reports {
         let file_name = format!("{}.png", report_name);
-        spreadsheet.save_png(&file_name).unwrap();
+        report.spreadsheet.save_png(&file_name).unwrap();
     }
 }
