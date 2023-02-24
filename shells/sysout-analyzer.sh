ad4d2462158014a01603788d22307573417af835
Jefferson Carlos <jeff.carlosbd@gmail.com>
2023-02-24 16:56:59 -0300

    desabilita casesensitive

--- shells/sysout-analyzer.sh
+++ shells/sysout-analyzer.sh
@@ -7,7 +7,7 @@ tg_chat_id=$4
 
 # gerar_links_martech_sh
 
-result=$(< "$(find /controlM/ctma9005/ctm/sysout/ -name "${script_name}*" -type f | sort -n | tail -1)" grep "${search_term}")
+result=$(< "$(find /controlM/ctma9005/ctm/sysout/ -name "${script_name}*" -type f | sort -n | tail -1)" grep -i "${search_term}")
 
 if [ "${result}" != "" ]; then
     /webtools/bot/tg "${tg_chat_id}" $'⚠️ ATENÇÃO ⚠️\n'"${message}"$'\n'"${result}"
