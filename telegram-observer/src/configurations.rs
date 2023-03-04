42d6f9e0361c1850fbadce8ed3132a86980b8061
Jefferson Carlos <jeff.carlosbd@gmail.com>
2023-03-04 00:45:14 -0300

    implementa validação de configurações

--- telegram-observer/src/configurations.rs
+++ telegram-observer/src/configurations.rs
@@ -1,13 +1,77 @@
-pub struct Configurations {}
+use std::fs;
+
+pub struct Command {
+    pub name: String,
+    pub script: String,
+    pub allowed_chats: Vec<i64>,
+}
+
+pub struct Configurations {
+    pub commands: Vec<Command>,
+}
 
 impl Configurations {
     pub fn from(mut args: impl Iterator<Item = String>) -> Result<Configurations, &'static str> {
         args.next();
 
-        if cfg!(debug_assertions) {
-            return Ok(Configurations {});
+        if !cfg!(debug_assertions) {
+            let commands = vec![Command {
+                name: String::from("/test"),
+                script: String::from(
+                    "/home/jefferson/projects/oi/shells/relatorio-das-cadeias.sh dev",
+                ),
+                allowed_chats: vec![5828637972],
+            }];
+            return Ok(Configurations { commands });
         }
 
-        Ok(Configurations {})
+        let mut commands: Vec<Command> = vec![];
+
+        for arg in args {
+            let parts: Vec<&str> = arg.split("|").collect();
+            if parts.len() != 3 {
+                let feedback = [
+                    "A estrutura dos comandos devem ser separadas por \"|\":",
+                    "   comando|ID,ID,ID|/path/to/script.sh",
+                ];
+                return Err(string_to_static::parse(feedback.join("\n")));
+            };
+            if parts[0] == "" {
+                return Err("O comando deve ter um nome");
+            }
+            let mut allowed_chats: Vec<i64> = vec![];
+            for id in parts[1].split(",") {
+                let parsed: i64 = match id.parse() {
+                    Ok(id) => id,
+                    Err(_) => {
+                        let feedback = [
+                            "A lista de ID's devem ser de números inteiros separados por vírgula:",
+                            "   comando|ID,ID,ID|/path/to/script.sh",
+                        ];
+                        return Err(string_to_static::parse(feedback.join("\n")));
+                    }
+                };
+                allowed_chats.push(parsed);
+            }
+            if let Err(_) = fs::metadata(parts[2]) {
+                let feedback = format!("O script \"{}\" não foi encontrado", parts[2]);
+                return Err(string_to_static::parse(feedback));
+            };
+            commands.push(Command {
+                name: String::from(parts[0]),
+                script: String::from(parts[2]),
+                allowed_chats,
+            });
+        }
+
+        if commands.len() == 0 {
+            let feedback = [
+                "não foi informado nenhum comando, use o formato:",
+                "   comando|ID,ID,ID|/path/to/script.sh",
+            ];
+            return Err(string_to_static::parse(feedback.join("\n")));
+        }
+
+        Ok(Configurations { commands })
     }
 }
