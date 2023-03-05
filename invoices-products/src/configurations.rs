727d1b25b396d294568ef153e10f37e915effc2a
Jefferson Carlos <jeff.carlosbd@gmail.com>
2023-03-05 19:58:14 -0300

    faz a aplicação ler o .csv informado pelo usuário

--- invoices-products/src/configurations.rs
+++ invoices-products/src/configurations.rs
@@ -1,13 +1,22 @@
-pub struct Configurations {}
+pub struct Configurations {
+    pub csv_path: String,
+}
 
 impl Configurations {
     pub fn from(mut args: impl Iterator<Item = String>) -> Result<Configurations, &'static str> {
         args.next();
 
         if cfg!(debug_assertions) {
-            return Ok(Configurations {});
+            return Ok(Configurations {
+                csv_path: "result.csv".to_string(),
+            });
         }
 
-        Ok(Configurations {})
+        let csv_path = match args.next() {
+            Some(path) => path,
+            None => return Err("deve ser informado o caminho para o .csv (em $1)"),
+        };
+
+        Ok(Configurations { csv_path })
     }
 }
