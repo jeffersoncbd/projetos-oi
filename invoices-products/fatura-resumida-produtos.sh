135f8616c2cf28b8cf60b0511ed6cb471ae6ae21
Jefferson Carlos <jeff.carlosbd@gmail.com>
2023-03-06 03:47:02 -0300

    finaliza script de entrada

--- invoices-products/fatura-resumida-produtos.sh
+++ invoices-products/fatura-resumida-produtos.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-rm "$HOME/result-products.csv" 2> /dev/null
+filtro=$1
 
-source "$HOME/.credentials"
+# --------------- Variáveis --------------- #
+PROJECT_FOLDER="/webaplic/bot_status_faturas"
+TG_TOKEN="5600687475:AAFtM9o3pcnECnDCMeM42OC0Rb1etknjk_Q"
+# TG_DESTINATARY_ID="-1001780198496"
+TG_DESTINATARY_ID="5828637972" # Jefferson
+CSV_FILE="${PROJECT_FOLDER}/email_seguro_produtos.csv"
+SQL_FILE="${PROJECT_FOLDER}/queries/email_seguro_produtos.sql"
+# ----------------------------------------- #
 
-echo "@$HOME/.queries/products.sql $HOME/result-products.csv" | /oracle/app/product/11.2.0.4/bin/sqlplus -s opuser/$DB_PASS@prgedbd-p1 2> /dev/null
-sed -i '1d' "$HOME/result-products.csv"
-sed -i '2d' "$HOME/result-products.csv"
+# EXECUÇÃO DE QUERY NO BANCO DE DADOS COM SPOOL DE CSV
+source "${PROJECT_FOLDER}/.credentials"
+echo "@${SQL_FILE} ${CSV_FILE}" | /oracle/app/product/11.2.0.4/bin/sqlplus -s opuser/${DB_PASS}@prgedbd-p1 > /dev/null
+sed -i '1d' "${CSV_FILE}"
+sed -i '2d' "${CSV_FILE}"
+
+# EXECUÇÃO DA HABILIDADE DO BOT
+${PROJECT_FOLDER}/relatorios/email_seguro_produtos "${CSV_FILE}" "${TG_TOKEN}" "${TG_DESTINATARY_ID}" "${filtro}"
+
+rm -f "${CSV_FILE}" > /dev/null
